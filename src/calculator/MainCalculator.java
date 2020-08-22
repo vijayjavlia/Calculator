7c1b0bdf19cdbc49694f356d575d457ce848d2c3
vijay <vijaykumarjavlia@gmail.com>
2020-08-22 02:34:16 -0700

    intil commit

---
+++ src/calculator/MainCalculator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+import mainInterface.Calculator;
+
+/**
+ *
+ * @author vijay
+ */
+public class MainCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      Calculator cal= new Calculator();
+      cal.setVisible(true);
+//        System.out.println("hellp");
+    }
+    
+}
